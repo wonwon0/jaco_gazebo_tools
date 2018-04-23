5da4bab4fc0fd22e451cfc551693d03f20e8e0fd
wonwon0 <philippe.lebel008@sympatico.ca>
2018-04-23 07:53:48 -0400

    modif bash file realworld

--- launch_jaco_real_arm.sh
+++ launch_jaco_real_arm.sh
@@ -3,5 +3,5 @@
 # Starts gazebo simulator with a jaco robot configured, starts a server that read joints
 # input and a python script that controls the robot
 roslaunch jaco_on_table jaco_on_table_gazebo_controlled.launch \
-| rosrun joint_states_listener joint_states_listener.py \
+| rosrun joint_states_listener joint_states_listener_real_jaco.py \
 | python gazebo_realworld_linker.py real_world
